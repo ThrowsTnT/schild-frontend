9906a6cccbcb6be97c3f495d7fe2bfed57177560
Finn Tegeler <developing@finn-tegeler.de>
2022-04-09 17:15:38 +0200

    feat: added styling, theme mode and constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+// APP TEXT
+export const APP_TITLE = 'schild';
+export const APP_DESCRIPTION = 'Service for protecting Minecraft against attacks through mods.';
+export const FOOTER_TEXT = `(C) ${new Date().getFullYear()} `;
+// UI CONSTANTS
+export const FOOTER_HEIGHT = 30;
+export const HEADER_HEIGHT = 60;
+export const DRAWER_WIDTH = 250;
+// APP THEME
+export const DARK_MODE_THEME = 'dark';
+export const LIGHT_MODE_THEME = 'light';
