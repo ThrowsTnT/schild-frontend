01b5efb98ae55fa86277168e33e0bfff8376f0d0
Finn Tegeler <developing@finn-tegeler.de>
2022-04-10 20:53:27 +0200

    feat: added analyzer tabs and dialogs to main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,35 @@
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { FC, useState } from "react";
 import { Helmet } from "react-helmet";
+import { AnalyzerTabs } from "../components/Analyzer";
+import { TermsOfServiceDialog } from "../components/Content";
+import { PrivacyPolicyDialog } from "../components/Content/PrivacyPolicyDialog";
 import { APP_TITLE } from "../utils/constants";
 
-export const Main = () => {
-    return (
-        <>
+export const Main: FC = () => {
+  const [isTermsOfServiceOpened, setTermsOfServiceOpened] = useState(false);
+  const [isPrivacyPolicyOpened, setPrivacyPolicyOpened] = useState(false);
+
+  return (
+    <>
       <Helmet>
         <title>
           {APP_TITLE}
         </title>
       </Helmet>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Container maxWidth="md">
+          <Typography variant="h1" align="center" style={{ padding: "25px" }}>
+            {APP_TITLE}
+          </Typography>
+
+          <AnalyzerTabs handleTermsOfServiceClicked={() => setTermsOfServiceOpened(true)} handlePrivacyPolicyClicked={() => setPrivacyPolicyOpened(true)} />
+        </Container>
+      </Box>
+
+      <PrivacyPolicyDialog open={isPrivacyPolicyOpened} onClose={() => setPrivacyPolicyOpened(false)} />
+      <TermsOfServiceDialog open={isTermsOfServiceOpened} onClose={() => setTermsOfServiceOpened(false)} />
     </>
-    );
+  );
 };
