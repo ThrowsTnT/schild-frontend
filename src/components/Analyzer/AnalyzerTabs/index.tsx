5db16a385703490f9137b69b5266e44282e3490b
Finn Tegeler <developing@finn-tegeler.de>
2022-04-11 20:37:08 +0200

    feat: added file input and updated text in analyzer tabs

--- src/components/Analyzer/AnalyzerTabs/index.tsx
+++ src/components/Analyzer/AnalyzerTabs/index.tsx
@@ -1,7 +1,6 @@
-import { Box, Tabs, Tab, Typography, TextField, Button, Stack, Link as MaterialLink } from "@mui/material";
+import { Box, Tabs, Tab, Typography, TextField, Button, Link as MaterialLink, styled } from "@mui/material";
 import React from "react";
 import { FunctionComponent, useState } from "react";
-import { Link } from "react-router-dom";
 
 type AnalyzerTabProps = {
     currentTab: string;
@@ -29,14 +28,22 @@ type AnalyzerTabsProps = {
     handlePrivacyPolicyClicked: () => void;
 };
 
-export const AnalyzerTabs: FunctionComponent<AnalyzerTabsProps> = ({handleTermsOfServiceClicked, handlePrivacyPolicyClicked}) => {
+const Input = styled('input')({
+    display: 'none',
+});
+
+export const AnalyzerTabs: FunctionComponent<AnalyzerTabsProps> = ({ handleTermsOfServiceClicked, handlePrivacyPolicyClicked }) => {
     const [currentTab, setCurrentTab] = useState("file");
 
-    const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+    const [selectedFiles, setSelectedFiles] = useState<FileList>();
     const [selectedURL, setSelectedURL] = useState("");
 
     const handleSelectedURLChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSelectedURL(event.target.value);
+    };
+
+    const handleSelectedFilesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedFiles(event.target.files ?? undefined);
     };
 
     return (
@@ -49,13 +56,28 @@ export const AnalyzerTabs: FunctionComponent<AnalyzerTabsProps> = ({handleTermsO
             </Box>
 
             <AnalyzerTab currentTab={currentTab} tab="file">
+                <Box px={3}>
+                    <Box my={1}>
+                        <Typography variant="body1" align="center">
+                            Add the file of a Minecraft modification to start the analysis. Our service will upload your file to our server and process it. Please keep this page open until the analysis is completed. 
+                        </Typography>
+                    </Box>
 
+                    <Box my={1} display="flex" justifyContent="center">
+                        <label htmlFor="add-file">
+                            <Input accept=".jar" id="add-file" type="file" onChange={handleSelectedFilesChange} />
+                            <Button variant="contained" component="span">
+                                Analyze from File
+                            </Button>
+                        </label>
+                    </Box>
+                </Box>
             </AnalyzerTab>
             <AnalyzerTab currentTab={currentTab} tab="link">
                 <Box px={3}>
                     <Box my={1}>
                         <Typography variant="body1" align="center">
-                            Enter a URL that directly leads to the JAR file you want to analyze. It should end in <b>.jar</b>
+                            Add the URL to the file of a Minecraft modification and click the button to start the analysis. Our service will try to download the modification from the given URL and process it. Please keep this page open until the analysis is completed.
                         </Typography>
                     </Box>
 
@@ -64,16 +86,16 @@ export const AnalyzerTabs: FunctionComponent<AnalyzerTabsProps> = ({handleTermsO
                     </Box>
 
                     <Box display="flex" justifyContent="center" my={1}>
-                    <Button variant="outlined">
+                        <Button variant="outlined">
                             Analyze from URL
                         </Button>
                     </Box>
                 </Box>
             </AnalyzerTab>
 
-            <Box px={3} my={1}>
+            <Box px={3} my={1} textAlign="center">
                 <Typography variant="caption">
-                    When you submit a file you agree to your <MaterialLink style={{cursor: "pointer"}} onClick={handleTermsOfServiceClicked}>Terms of Service</MaterialLink> and <MaterialLink style={{cursor: "pointer"}} onClick={handlePrivacyPolicyClicked}>Privacy Policy</MaterialLink>. 
+                    When you submit a file you agree to your <MaterialLink style={{ cursor: "pointer" }} onClick={handleTermsOfServiceClicked}>Terms of Service</MaterialLink> and <MaterialLink style={{ cursor: "pointer" }} onClick={handlePrivacyPolicyClicked}>Privacy Policy</MaterialLink>.
                     The modification can be used by us for further research in the scope of this project. Potential security issues might be published while the contents of the modification will never be published by us.
                 </Typography>
             </Box>
